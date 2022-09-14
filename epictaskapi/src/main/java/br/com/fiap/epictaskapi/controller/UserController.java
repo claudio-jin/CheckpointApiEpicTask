1b036958dd95dcc05f2ee9de58d6e23447ca5f47
Claudio Jin <natsu165@hotmail.com>
2022-09-14 15:48:55 -0300

    add get set id

--- epictaskapi/src/main/java/br/com/fiap/epictaskapi/controller/UserController.java
+++ epictaskapi/src/main/java/br/com/fiap/epictaskapi/controller/UserController.java
@@ -1,11 +1,21 @@
 package br.com.fiap.epictaskapi.controller;
 
+import javax.persistence.Cacheable;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +30,8 @@ public class UserController {
     @Autowired
     private UserService service;
     
+
+    //cadastro de usuarios
     @PostMapping
     public ResponseEntity<User> create(@RequestBody @Valid User user) {
         service.save(user);
@@ -27,5 +39,38 @@ public class UserController {
                 .status(HttpStatus.CREATED)
                 .body(user);
     }
+
+    //listagem de usuarios
+    @GetMapping
+    @PreAuthorize("permitAll()")
+    public Page<User> index(@PageableDefault(size = 5) Pageable paginacao){
+        return service.listAll(paginacao);
+    }
+
+    @GetMapping("{id}")
+    @PreAuthorize("permitAll()")
+    public ResponseEntity<User> show(@PathVariable Long id){
+        return ResponseEntity.of(service.getById(id));
+    }
+
+    
+    @PutMapping("{id}")
+    @PreAuthorize("isAuthenticated()")
+    @CacheEvict(value = "user", allEntries = true)
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody @Valid User newUser){
+        var optional = service.getById(id);
+
+        if(optional.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        var user = optional.get();
+        BeanUtils.copyProperties(newUser, user);
+        user.setId(id);
+
+        service.save(user);
+        return ResponseEntity.ok(user);
+    }
+
+
 
 }
